7b1d3939714b7e44341ce458e70e38891d4df343
Martin Skočík <mskocik@gmail.com>
2018-05-01 08:53:40 +0200

    init auryn in constructor

--- src/Extensions/DIExtension.php
+++ src/Extensions/DIExtension.php
@@ -9,6 +9,7 @@ class DIExtension extends Nette\DI\CompilerExtension
 	public function afterCompile(\Nette\PhpGenerator\ClassType $class)
 	{
 		$constructor = $class->getMethod('__construct');
+		$constructor->addBody('$this->initAuryn();');
 		$constructor->addBody('static::$self = $this;');
 		parent::afterCompile($class);
 	}
