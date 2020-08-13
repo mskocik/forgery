3bbf39d6b4897077a2fe2b585536461d0a2986c4
Martin Skočík <mskocik@gmail.com>
2020-08-13 07:13:05 +0200

    iterate over aliases

--- src/Forgery.php
+++ src/Forgery.php
@@ -19,7 +19,9 @@ trait Forgery {
             unset($args[Container::FORGERY_DEFINE]);
         }
         if (isset($args[Container::FORGERY_ALIAS])) {
-            $this->aurynContainer->addAurynAlias($className, $args[Container::FORGERY_ALIAS]);
+            foreach ($args[Container::FORGERY_ALIAS] as $original => $impl) {
+                $this->aurynContainer->addAurynAlias($original, $impl);
+            }
             unset($args[Container::FORGERY_ALIAS]);
         }
         return $this->aurynContainer->make($className, $args, $type);
